0e9516edb69d0007aefa60fabb135ba2d34c787d
Andrei Haiko <andreygayko@gmail.com>
2021-03-21 17:31:28 +0300

    Add AsyncStorage for data saving

--- App.js
+++ App.js
@@ -1,25 +1,80 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, TextInput, Button, Alert } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import { Serial } from './components/Serial';
-import { RoundButton } from './components/RoundButton';
+import { Serial } from './src/Serial';
+import { RoundButton } from './src/RoundButton';
 
 export default function App() {
-  const [serials, setSerials] = useState([{title: '', episode: 0}]);
+  const [serials, setSerials] = useState([]);
+  const [newSerial, setNewSerial] = useState(false);
+  const [title, setTitle] = useState('');
 
-  const addSerial = () => {
-     
+  useEffect(() => {
+    AsyncStorage.getAllKeys().then((keys) => {
+      return AsyncStorage.multiGet(keys)
+        .then((result) => {
+          const arr = result.map(el => ({title: el[0], episode: el[1]}));
+          setSerials(arr);
+        }).catch((e) =>{
+          Alert(e);
+        });
+    });
+  }, []);  
+  
+  const saveSerial = async (title, episode) => {
+    const arr = serials.map(el => ({title: title, episode: episode}));
+    try {
+      await AsyncStorage.setItem(title, episode);
+    } catch (e) {
+      Alert(e);
+    }
+  };
+
+  const handleAddSerial = () => {
+    if (title) {
+      const arr = [...serials];
+      arr.push({title, episode: 0});
+      saveSerial(title, '0');
+      setSerials(arr);
+      setNewSerial(false);
+      setTitle('');
+    }
+  };
+
+  const handleAddEpisode = (index) => {
+    const arr = [...serials];
+    const nextEpisode = parseInt(arr[index].episode) + 1;
+    arr[index].episode++;
+    setSerials(arr);
+    saveSerial(arr[index].title, nextEpisode.toString());
+  }
+
+  const handleSubstractEpisode = (index) => {
+    const arr = [...serials];
+    const nextEpisode = arr[index].episode - 1;
+    arr[index].episode--;
+    setSerials(arr);
+    saveSerial(arr[index].title, nextEpisode);
   }
   
   return (
     <View style={{flex: 1}}>
     <View style={styles.container}>
-      {serials.map((el, i) => (<Serial key={i} title={el.title} episode={el.episode}/>))}
-      <TextInput placeholder='Title' style={{borderBottomColor: 'black', borderBottomWidth: 1, width: '50%', marginTop: 20, height: 22}}/>   
+      {serials.map((el, i) => (<Serial key={i} title={el.title} episode={el.episode} index={i} handleAddEpisode={handleAddEpisode} handleSubstractEpisode={handleSubstractEpisode}/>))}
+
+      {newSerial && 
+      <View style={{width: '50%'}}>
+      <TextInput placeholder='Title' onChangeText={setTitle} style={{borderBottomColor: 'black', borderBottomWidth: 1, marginTop: 20, height: 22}}/>   
+      <Button title='Add' onPress={handleAddSerial}/>
+      </View>}
     </View>
+
+    {!newSerial &&
     <View style={styles.btnAdd}>
-      <RoundButton title='+' onPress={addSerial} />
+      <RoundButton title='+' onPress={() => setNewSerial(true)}/>
       </View>
+    }
     </View>
   );
 }
--- src/Serial.js
+++ src/Serial.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
 export const Serial = (props) => {
-  const {title, episode} = props;
+  const {title, episode, handleAddEpisode, handleSubstractEpisode, index} = props;
   
   return (
     <View style={styles.container}>
       <Text style={[styles.text, styles.item]}>{title}:</Text>
-      <Button title='-' style={styles.item}/>
+      <Button title='-' onPress={() => handleSubstractEpisode(index)} style={styles.item}/>
       <Text style={styles.item}>{episode}</Text>
-      <Button title='+' style={styles.item}/>
+      <Button title='+' onPress={() => handleAddEpisode(index)} style={styles.item}/>
     </View>
   )
 };
