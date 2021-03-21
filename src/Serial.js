991ab86877b3b6fdd1856b393d4602af924c8fbf
Andrei Haiko <andreygayko@gmail.com>
2021-03-21 19:31:13 +0300

    Add styles to list

--- src/Serial.js
+++ src/Serial.js
@@ -7,21 +7,26 @@ export const Serial = (props) => {
   return (
     <View style={styles.container}>
       <Text style={[styles.text, styles.item]}>{title}:</Text>
+      <View style={{ flexDirection: 'row'}}>
       <Button title='-' onPress={() => handleSubstractEpisode(index)} style={styles.item}/>
       <Text style={styles.item}>{episode}</Text>
       <Button title='+' onPress={() => handleAddEpisode(index)} style={styles.item}/>
+      </View>
     </View>
   )
 };
 
 const styles = StyleSheet.create({
     container: {
+      padding: 10,
       width: '90%',
       flexDirection: "row",
       alignItems: 'center',
+      borderBottomWidth: 1,
     },
     text: {
-      maxWidth: '70%',
+      maxWidth: '80%',
+      flex: 1,
     },
     item: {
         padding: 10,
